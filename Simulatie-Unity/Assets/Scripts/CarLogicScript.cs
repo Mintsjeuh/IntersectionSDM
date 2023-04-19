e52d976a3a3d493fdf4b0129d58e376953543848
Cynthia <cynthia.wiersma@student.nhlstenden.com>
2023-04-19 15:28:07 +0200

    Added basic connection code

--- Simulatie-Unity/Assets/Scripts/CarLogicScript.cs
+++ Simulatie-Unity/Assets/Scripts/CarLogicScript.cs
@@ -5,57 +5,56 @@ using UnityEngine;
 public class CarLogicScript : MonoBehaviour
 {
     [SerializeField]
-    float accelerationPower = 2.5f;
-    [SerializeField]
-    float steeringPower = 2.5f;
-    float steeringAmount, speed, direction;
+    float moveSpeed = 2f;
 
-    public float deadZoneLeft = -25;
-    public float deadZoneRight = 25;
+    int waypointIndex = 0;
 
     public Rigidbody2D myRigidbody;
+    private Transform[] _waypoints;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float deadzoneLeftSide = -25;
+    float deadzoneRightSide = 32;
+    float deadzoneTopSide = 10;
+    float deadzoneBottomSide = -11;
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < deadZoneLeft)
+        if (_waypoints.Length != 0)
         {
-            Debug.Log("Car left deleted");
+            Move(_waypoints);
+        }
+
+        if (transform.position.x < deadzoneLeftSide)
+        {
             Destroy(gameObject);
         }
-        else if (transform.position.x > deadZoneRight)
+        if (transform.position.x > deadzoneRightSide)
         {
-            Debug.Log("Car right deleted");
+            Destroy(gameObject);
+        }
+        if (transform.position.y > deadzoneTopSide)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.y < deadzoneBottomSide)
+        {
             Destroy(gameObject);
         }
     }
 
-    // Frame-rate independent for physics calculations
-    void FixedUpdate()
+    public void Move(Transform[] waypoints)
     {
-        // Kijkt of de linker of rechter arrowkey is ingedrukt als negatieve waarde
-        steeringAmount = - Input.GetAxis("Horizontal");
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
 
-        // Kijkt of de up of down arrowkey is ingedrukt, vermenigvuldigd met de acceleratie power
-        speed = Input.GetAxis("Vertical") * accelerationPower;
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+    }
 
-        // De richting wordt berekend
-        // Vector2.Dot --> returnt een positieve waarde als de vectoren in de zelfde richting wijzen, anders een negatieve waarde
-        // Mathf.Sign --> returnt 1 als hij positief of 0 is en -1 als hij negatief is
-        direction = Mathf.Sign(Vector2.Dot(myRigidbody.velocity, myRigidbody.GetRelativeVector(Vector2.up)));
-        myRigidbody.rotation += steeringAmount * steeringPower * myRigidbody.velocity.magnitude * direction;
-
-        // Beweegt een auto vooruit of achteruit
-        myRigidbody.AddRelativeForce(Vector2.right * speed);
-
-        // Beweegt een auto links en rechts
-        myRigidbody.AddRelativeForce( - Vector2.right * myRigidbody.velocity.magnitude * steeringAmount / 2);
+    public void SetWaypoints(Transform[] waypoints)
+    {
+        _waypoints = waypoints;
     }
 }
