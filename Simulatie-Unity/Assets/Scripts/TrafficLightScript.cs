8bc19cf1df916a091d09cc861098b3869920873d
Cynthia <cynthia.wiersma@student.nhlstenden.com>
2023-04-24 12:19:04 +0200

    Added basic connection code

--- Simulatie-Unity/Assets/Scripts/TrafficLightScript.cs
+++ Simulatie-Unity/Assets/Scripts/TrafficLightScript.cs
@@ -1,39 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using lightNamespace;
+using System;
 
 public class TrafficLightScript : MonoBehaviour
 {
     public SpriteRenderer mySpriteRenderer;
-    enum TrafficLightStatus{
-        Red,
-        Orange,
-        Green
-    }
+    public double lightId;
+    public TrafficLightStatus status;
 
-    // default state of red
-    TrafficLightStatus status = TrafficLightStatus.Red;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // For testing
+    private float _timer = 0;
+    private float _timeUntilChange = 4;
 
     // Update is called once per frame
     void Update()
     {
-        switch (status)
+        if (_timer < _timeUntilChange)
         {
-            case TrafficLightStatus.Red:
-                mySpriteRenderer.color = Color.red;
-                break;
-            case TrafficLightStatus.Orange:
-                mySpriteRenderer.color = new Color(255, 165, 0);
-                break;
-            case TrafficLightStatus.Green: 
-                mySpriteRenderer.color = Color.green; 
-                break;
+            _timer = _timer + Time.deltaTime;
         }
+        else
+        {
+            ChangeTrafficLightColor();
+            _timer = 0;
+        }
+    }
+
+    public TrafficLightStatus ChangeTrafficLightColor()
+    {
+        // For testing: generates a random color and changes the sprite color to that color
+        Array values = Enum.GetValues(typeof(TrafficLightStatus));
+        System.Random random = new System.Random();
+        TrafficLightStatus randomStatus = (TrafficLightStatus)values.GetValue(random.Next(values.Length));
+
+        if (randomStatus == TrafficLightStatus.Red)
+        {
+            mySpriteRenderer.color = Color.red;
+        }
+
+        if (randomStatus == TrafficLightStatus.Orange)
+        {
+            mySpriteRenderer.color = new Color(255, 165, 0);
+        }
+
+        if (randomStatus == TrafficLightStatus.Green)
+        {
+            mySpriteRenderer.color = Color.green;
+        }
+
+        return randomStatus;
     }
 }
